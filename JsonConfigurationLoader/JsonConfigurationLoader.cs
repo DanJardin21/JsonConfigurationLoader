f3191a2dfd8a4d21223abfb6d785c221a2f4c4f4
DanJardin21 <131458913+DanJardin21@users.noreply.github.com>
2023-04-21 15:25:41 +0200

    Initial Commit
    My First Github Commit

---
+++ JsonConfigurationLoader/JsonConfigurationLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace JsonConfigurationLoader
+{
+    public class JsonConfigurationLoader
+    {
+        private readonly JsonElement _config;
+
+        public JsonConfigurationLoader(string configFilePath)
+        {
+            if (string.IsNullOrEmpty(configFilePath))
+            {
+                throw new ArgumentException("Configuration file path cannot be null or empty.", nameof(configFilePath));
+            }
+
+            if (!File.Exists(configFilePath))
+            {
+                throw new FileNotFoundException("Configuration file not found.", configFilePath);
+            }
+
+            string configContent = File.ReadAllText(configFilePath);
+            _config = JsonSerializer.Deserialize<JsonElement>(configContent);
+        }
+
+        public T GetValue<T>(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Key cannot be null or empty.", nameof(key));
+            }
+
+            if (_config.TryGetProperty(key, out JsonElement value))
+            {
+                return JsonSerializer.Deserialize<T>(value.GetRawText());
+            }
+
+            throw new KeyNotFoundException($"Key '{key}' not found in configuration.");
+        }
+    }
+}
